4e2910fb83070890e36f0ca92274adaf37e696f3
majoseek <matuslaw.mati@gmail.com>
2021-11-29 23:34:24 +0100

    Fixed navbar

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,68 +1,20 @@
 import "./Navigation.css";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
 export const Navigation = () => {
     return (
-        <nav id="menu" className="navbar navbar-default fixed-top">
-            <div className="container">
-                <div>
-                    <button
-                        type="button"
-                        className="navbar-toggler"
-                        data-toggle="collapse"
-                        data-target="#menu_collapse"
-                    >
-                        {" "}
-                        <span className="d-md-none">
-                            Toggle navigation
-                        </span>{" "}
-                        <span className="icon-bar"></span>{" "}
-                        <span className="icon-bar"></span>{" "}
-                        <span className="icon-bar"></span>{" "}
-                    </button>
-                    <a className="navbar-brand page-scroll" href="#page-top">
-                        React Landing Page
-                    </a>{" "}
-                </div>
-
-                <div className="collapse navbar-collapse" id="menu_collapse">
-                    <ul className="nav navbar-nav navbar-right">
-                        <li>
-                            <a href="#features" className="page-scroll">
-                                Features
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#about" className="page-scroll">
-                                About
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#services" className="page-scroll">
-                                Services
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#portfolio" className="page-scroll">
-                                Gallery
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#testimonials" className="page-scroll">
-                                Testimonials
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#team" className="page-scroll">
-                                Team
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#contact" className="page-scroll">
-                                Contact
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <Navbar bg="light" expand="lg" fixed="top">
+            <Container>
+                <Navbar.Brand href="#home">React Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
