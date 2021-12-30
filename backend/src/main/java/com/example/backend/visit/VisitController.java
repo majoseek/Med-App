53048700cbcd1a620cd5027e30f4267efcc03de4
Julia Jodczyk <01158711@pw.edu.pl>
2021-12-30 21:33:48 +0100

    endpoint for counting visits by month and doctor

--- backend/src/main/java/com/example/backend/visit/VisitController.java
+++ backend/src/main/java/com/example/backend/visit/VisitController.java
@@ -78,11 +78,19 @@ public class VisitController {
     @ResponseBody
     public ResponseEntity<?> getDoctorVisit(@PathVariable Long doctorId) {
         try {
-            List<VisitDto> patientsVisits = service.getDoctorVisits(doctorId).stream().map(this::convertToDto).collect(Collectors.toList());
-            return ResponseEntity.ok(patientsVisits);
+            List<VisitDto> doctorsVisits = service.getDoctorVisits(doctorId).stream().map(this::convertToDto).collect(Collectors.toList());
+            return ResponseEntity.ok(doctorsVisits);
         } catch (UserNotFound userNotFound) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(userNotFound.getLocalizedMessage());
         }
+    }
+
+    @GetMapping("/doctor/{doctorId}/countVisits")
+    @ResponseBody
+    public ResponseEntity<?> getVisitCountByMonth(@PathVariable Long doctorId,
+                                                  @RequestBody Map<String, String> month) {
+        Integer visitCount = service.getVisitCountByMonth(doctorId, month);
+        return ResponseEntity.ok(visitCount);
     }
 
     @PutMapping("/visits/updateDate/{visitId}")
--- backend/src/main/java/com/example/backend/visit/VisitRepository.java
+++ backend/src/main/java/com/example/backend/visit/VisitRepository.java
@@ -2,12 +2,14 @@ package com.example.backend.visit;
 
 import com.example.backend.doctor.Doctor;
 import com.example.backend.patient.Patient;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.sql.Date;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 
 public interface VisitRepository extends CrudRepository<Visit, Long> {
     List<Visit> findAllBy();
@@ -19,4 +21,7 @@ public interface VisitRepository extends CrudRepository<Visit, Long> {
     List<Visit> findAllByDateBetween(LocalDateTime start, LocalDateTime end);
 
     List<Visit> findAllByLocation(String location);
+
+    @Query(value = "select count(v) from Visit v where v.doctorByDoctorUserId.userId = :doctorId and SUBSTRING(v.date, 4, 2) = :month")
+    Integer countVisitByMonthAndDoctor(Long doctorId, Map<String, String> month);
 }
