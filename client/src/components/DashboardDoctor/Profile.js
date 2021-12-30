083ac749e8bdb28f83d6d79a2eca96c6d0be4a39
majoseek <matuslaw.mati@gmail.com>
2021-12-30 16:28:27 +0100

    Added profile grid item rendering

--- client/src/components/DashboardDoctor/Profile.js
+++ client/src/components/DashboardDoctor/Profile.js
@@ -1,3 +1,54 @@
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Title from "./Title";
+import { TextField } from "@mui/material";
 export default function Profile() {
-    return <h1>Profile</h1>;
+    return (
+        <Grid container spacing={3}>
+            <Grid item xs={12}>
+                <Paper
+                    sx={{
+                        p: 2,
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        "& .MuiTextField-root": { m: 1, width: "25ch" },
+                    }}
+                >
+                    <Title>My profile</Title>
+                    <Grid
+                        container
+                        spacing={3}
+                        direction="row"
+                        alignItems="center"
+                        justifyContent="center"
+                    >
+                        <Grid item>
+                            <TextField
+                                id="standard-read-only-input"
+                                label="Name"
+                                defaultValue="Jan"
+                                InputProps={{
+                                    readOnly: true,
+                                }}
+                                variant="standard"
+                            />
+                        </Grid>
+                        <Grid item>
+                            <TextField
+                                id="standard-read-only-input"
+                                label="Surname"
+                                defaultValue="Kowalski"
+                                InputProps={{
+                                    readOnly: true,
+                                }}
+                                variant="standard"
+                            />
+                        </Grid>
+                    </Grid>
+                </Paper>
+            </Grid>
+        </Grid>
+    );
 }
